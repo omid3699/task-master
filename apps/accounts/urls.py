4613b8515c5c0599fd3bdb596c642f6dcecbb5c6
omid <omedmm3@gmail.com>
2025-02-05 08:19:43 +0000

    Added View For getting user

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import SignUpView
+from .views import GetUserView, SignUpView
 
 app_name = "accounts"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", GetUserView.as_view(), name="get_user"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import UserSerializer
 
@@ -6,3 +9,11 @@ from .serializers import UserSerializer
 # Endpoint for user signup
 class SignUpView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class GetUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request) -> Response:
+        user = UserSerializer(request.user)
+        return Response(data=user.data, status=200)
